68c9575e750a1d6f1c41ead386bd4a15c96c5307
furkiti <furkanbdr99@hotmail.com>
2020-02-23 03:09:44 +0300

    dosyalar eklendi

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>EXTUR - Her Türlü Tur Hizmeti</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+
+  <?php
+
+  require_once("piecesofpages/upperbar.php");
+
+  ?>
+
+
+  <header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <!-- Slide One - Set the background image for this slide in the line below -->
+        <div class="carousel-item active" style="background-image: url('img/Switzerland/00.jpg')">
+          <div class="carousel-caption d-none d-md-block">
+            <div class="row">
+              <div class="col"></div>
+              <div class="col" style="background: rgba(204, 204, 204, 0.5);">
+                <a href="#" style="text-decoration:none;">
+
+                  <h3 style="color:black; font-size:2.5em;">İsviçre Turları</h3>
+                  <p style="color:black; text-"><b style="filter:opacity(0.7);">İsviçre'nin Dağlarına Yolculuk</b></p>
+                </a>
+              </div>
+
+              <div class="col"></div>
+
+            </div>
+
+          </div>
+        </div>
+         Slide Two - Set the background image for this slide in the line below -->
+         <div class="carousel-item" style="background-image: url('img/Norway/01.jpg')">
+          <div class="carousel-caption d-none d-md-block">
+            <div class="row">
+              <div class="col"></div>
+              <div class="col" style="background: rgba(204, 204, 204, 0.5);">
+                <a href="#" style="text-decoration:none;">
+
+                  <h3 style="color:black; font-size:2.5em;">Norveç Turları</h3>
+                  <p style="color:black; text-"><b style="filter:opacity(0.7);">Kuzey Işıklarını Keşfedin...</b></p>
+                </a>
+              </div>
+
+              <div class="col"></div>
+
+            </div>
+          </div>
+        </div>
+         Slide Three - Set the background image for this slide in the line below -->
+        <div class="carousel-item" style="background-image: url('img/Prague/00.jpg')">
+          <div class="carousel-caption d-none d-md-block">
+            <div class="row">
+              <div class="col"></div>
+              <div class="col" style="background: rgba(204, 204, 204, 0.5);">
+                <a href="#" style="text-decoration:none;">
+
+                  <h3 style="color:black; font-size:2.5em;">Balkan Turları</h3>
+                  <p style="color:black; text-"><b style="filter:opacity(0.7);">Sanat Dolu Ülkeler</b></p>
+                </a>
+              </div>
+
+              <div class="col"></div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </header>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <h1 class="my-4" style="text-align:center;">EN ÇOK TERCİH EDİLEN TURLARIMIZ</h1>
+    <hr>
+
+
+
+    <!-- Portfolio Section -->
+    <h2>Yurtiçi <small style="font-size:0.6em; color: gray;">(En Çok Tercih Edilen Yurtiçi Turlarımız)</small></h2>
+
+    <hr>
+
+    <div class="row">
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="img/Ankara/00.jpg" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Ankara Turu</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="img/İstanbul/00.jpg" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">İstanbul Turu</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="img/İzmir/01.jpg" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">İzmir Turu</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+    <h2>Yurtdışı <small style="font-size:0.6em; color: gray;">(En Çok Tercih Edilen Yurtdışı Turlarımız)</small></h2>
+    <hr>
+
+    <div class="row">
+
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="img/Amsterdam/00.jpg" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Amsterdam Turu</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="img/Paris/00.jpg" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Paris Turu</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="img/Londra/00.jpg" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Londra Turu</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+
+
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <?php
+
+  require_once("piecesofpages/footer.php");
+
+
+   ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
