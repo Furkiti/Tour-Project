3b5cf95f11723e6c9746576e93ee0031d1f04b7a
Furkan Bedir <furkanbdr99@hotmail.com>
2020-02-25 00:20:28 +0300

    iletişim sayfası tamamlandı

--- contact.php
+++ contact.php
@@ -30,15 +30,13 @@
   <div class="container">
 
     <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3">Contact
-      <small>Subheading</small>
-    </h1>
+    <h1 class="mt-4 mb-3">İletişim</h1>
 
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="index.html">Home</a>
+        <a href="index.php">Ana Sayfa</a>
       </li>
-      <li class="breadcrumb-item active">Contact</li>
+      <li class="breadcrumb-item active">İletişim</li>
     </ol>
 
     <!-- Content Row -->
@@ -46,69 +44,93 @@
       <!-- Map Column -->
       <div class="col-lg-8 mb-4">
         <!-- Embedded Google Map -->
-        <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+
+
+        <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.82427744723!2d32.84898400136699!3d39.92294847932454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34effd6b9930b%3A0x6fa79cbeeeec0bac!2zS8SxesSxbGF5LCAwNjQyMCDDh2Fua2F5YS9BbmthcmE!5e0!3m2!1str!2str!4v1582578044510!5m2!1str!2str"></iframe>
+        <br>
+        <br>
+        <br>
+        <!-- Mesaj Gönderme Bölümü -->
+        <div class="row">
+          <div class="col-lg-8 mb-4">
+            <h3>Bize Mesaj Gönderin!</h3>
+            <hr>
+            <form name="sentMessage" id="contactForm" novalidate>
+              <div class="control-group form-group">
+                <div class="controls">
+                  <label>Adınız</label>
+                  <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
+                  <p class="help-block"></p>
+                </div>
+              </div>
+              <div class="control-group form-group">
+                <div class="controls">
+                  <label>Telefon Numaranız</label>
+                  <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
+                </div>
+              </div>
+              <div class="control-group form-group">
+                <div class="controls">
+                  <label>Email Adresiniz</label>
+                  <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+                </div>
+              </div>
+              <div class="control-group form-group">
+                <div class="controls">
+                  <label>Mesajınız</label>
+                  <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+                </div>
+              </div>
+              <div id="success"></div>
+              <!-- For success/fail messages -->
+              <button type="submit" class="btn btn-primary" id="sendMessageButton">Mesajı Gönder</button>
+            </form>
+          </div>
+
+        </div>
       </div>
       <!-- Contact Details Column -->
       <div class="col-lg-4 mb-4">
-        <h3>Contact Details</h3>
+        <h3>İletişim Detayları</h3>
+        <hr>
         <p>
-          3481 Melrose Place
-          <br>Beverly Hills, CA 90210
-          <br>
+          Kızılay
+            06420 Çankaya/Ankara
         </p>
         <p>
-          <abbr title="Phone">P</abbr>: (123) 456-7890
-        </p>
-        <p>
-          <abbr title="Email">E</abbr>:
-          <a href="mailto:name@example.com">name@example.com
+          <abbr title="Email">Email</abbr>:
+          <a href="mailto:name@example.com">furkanbdr99@hotmail.com
           </a>
         </p>
         <p>
-          <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM
+          <abbr title="Hours">Çalışma Saatleri</abbr>: Pazartesi - Cuma: 9:00 - 17:00
         </p>
+        <br>
+        <br>
+        <h3>Danışmanlarımız</h3>
+        <hr>
+          <div class="card" style="width: 18rem;">
+          <img src="https://thispersondoesnotexist.com/image" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Ayşe Öz</p>
+          </div>
+        </div>
+        <hr>
+        <div class="card" style="width: 18rem;">
+        <img src="https://thispersondoesnotexist.com/image" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Ahmet Dere</p>
+        </div>
       </div>
+      </div>
+
+
     </div>
     <!-- /.row -->
 
     <!-- Contact Form -->
     <!-- In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-    <div class="row">
-      <div class="col-lg-8 mb-4">
-        <h3>Send us a Message</h3>
-        <form name="sentMessage" id="contactForm" novalidate>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Full Name:</label>
-              <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
-              <p class="help-block"></p>
-            </div>
-          </div>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Phone Number:</label>
-              <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
-            </div>
-          </div>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Email Address:</label>
-              <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
-            </div>
-          </div>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Message:</label>
-              <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
-            </div>
-          </div>
-          <div id="success"></div>
-          <!-- For success/fail messages -->
-          <button type="submit" class="btn btn-primary" id="sendMessageButton">Send Message</button>
-        </form>
-      </div>
 
-    </div>
     <!-- /.row -->
 
   </div>
