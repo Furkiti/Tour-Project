dfef2cf5d9a5959cad428cfaf6487557fe4a5b06
furkiti <furkanbdr99@hotmail.com>
2020-03-06 01:16:09 +0300

    amsterdam turu

--- amsterdam.php
+++ amsterdam.php
@@ -12,6 +12,11 @@
 
     <!-- Custom styles for this template -->
     <link href="css/modern-business.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/amsterdam.css">
+    <!-- <link rel="stylesheet" href="css/index.css"> -->
+
+
   </head>
   <body>
 
@@ -21,6 +26,137 @@
 
     ?>
 
+
+    <div class="container mainpage">
+
+      <div class="main tours">
+
+        <h1>Amsterdam Turu <small style="font-size:60%; color:gray;">(2 Gece 3 Gün)</small> </h1>
+        <br>
+
+        <div class="row">
+
+          <div class="col-8">
+
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" width="800" height="500">
+                  <div class="carousel-inner" style="width:730px; height:456px;">
+                    <div class="carousel-item active">
+                      <img src="img/Amsterdam/00.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="img/Amsterdam/01.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="https://via.placeholder.com/800x500" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+          </div>
+
+
+
+          </div>
+
+          <div class="col-4 info">
+
+            <h3 style="margin-top:0.6em;">Hizmetlerimiz</h3>
+            <div class="services" style="margin-top:0.5em; padding-top:0.5em;">
+
+              <i class="fas fa-plane" alt=""> <span style="padding-left:1em;">Uçak Hizmetleri</span> </i>
+              <i class="fas fa-utensils"><span style="padding-left:1em;">Yemek Hizmetleri</span></i>
+              <i class="fas fa-home"><span style="padding-left:1em;">Konaklama Hizmetleri</span></i>
+              <i class="fas fa-question"><span style="padding-left:1em;">Rehber Hizmetleri</span></i>
+
+            </div>
+
+
+
+            <div class="rezervevesatinal">
+
+              <button type="button" class="btn btn-warning">Rezervasyon Yap</button>
+
+              <button type="button" class="btn btn-info price" style="position:relative;">
+
+                  <span class="old-price">2.500 TL</span>
+
+                  <div class="row current">
+                      <span class="current-price">1.799,00</span>
+                      <span class="current-currency">TL</span>
+
+                  </div>
+              </button>
+              <button type="button" class="taksit btn btn-info">Taksit Seçenekleri</button>
+
+            </div>
+
+
+
+
+
+
+          </div>
+
+
+
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+
+            <div class="card text-center tourinfo">
+                      <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                          <li class="nav-item">
+                            <a class="nav-link active" href="#">Tur Bilgileri</a>
+                          </li>
+
+                        </ul>
+                      </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Amsterdam Turu <small style="font-size:60%; color:gray;">(2 Gece 3 Gün)</small></h5>
+                        <p class="card-text">Lorem Ipsum Nedir?
+                              Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+
+                              Neden Kullanırız?
+                              Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.
+
+
+                              Nereden Gelir?
+                              Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz. Kökleri M.Ö. 45 tarihinden bu yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır. Virginia'daki Hampden-Sydney College'dan Latince profesörü Richard McClintock, bir Lorem Ipsum pasajında geçen ve anlaşılması en güç sözcüklerden biri olan 'consectetur' sözcüğünün klasik edebiyattaki örneklerini incelediğinde kesin bir kaynağa ulaşmıştır. Lorm Ipsum, Çiçero tarafından M.Ö. 45 tarihinde kaleme alınan "de Finibus Bonorum et Malorum" (İyi ve Kötünün Uç Sınırları) eserinin 1.10.32 ve 1.10.33 sayılı bölümlerinden gelmektedir. Bu kitap, ahlak kuramı üzerine bir tezdir ve Rönesans döneminde çok popüler olmuştur. Lorem Ipsum pasajının ilk satırı olan "Lorem ipsum dolor sit amet" 1.10.32 sayılı bölümdeki bir satırdan gelmektedir.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+
+      </div>
+
+
+
+
+    </div>
+
+
+
+    <?php
+
+    require_once("inc_footer.html");
+
+
+     ?>
 
 
 
